e13326c0aa046a829b02e77679311f859e78253c
Shariar Arafat Tamim <141410363+shariarcode@users.noreply.github.com>
2025-08-01 14:56:12 +0600

    Fixed bug in login form

--- src/services/database.types.ts
+++ src/services/database.types.ts
@@ -154,41 +154,7 @@ export interface Database {
     Functions: {
       handle_new_user: {
         Args: Record<PropertyKey, never>
-        Returns: {
-          id: string
-          aud: string
-          role: string
-          email: string
-          encrypted_password: string
-          email_confirmed_at: string
-          invited_at: string
-          confirmation_token: string
-          confirmation_sent_at: string
-          recovery_token: string
-          recovery_sent_at: string
-          email_change_token_new: string
-          email_change: string
-          email_change_sent_at: string
-          last_sign_in_at: string
-          raw_app_meta_data: Json
-          raw_user_meta_data: Json
-          is_super_admin: boolean
-          created_at: string
-          updated_at: string
-          phone: string
-          phone_confirmed_at: string
-          phone_change: string
-          phone_change_token: string
-          phone_change_sent_at: string
-          email_change_token_current: string
-          email_change_confirm_status: number
-          banned_until: string
-          reauthentication_token: string
-          reauthentication_sent_at: string
-          is_sso_user: boolean
-          deleted_at: string
-          is_anonymous: boolean
-        }
+        Returns: void
       }
     }
     Enums: {
